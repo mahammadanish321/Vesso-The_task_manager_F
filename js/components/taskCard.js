f8f5ed8a8fb45a11167ef3bc11a6639797b85015
mahammadanish321 <anish130905@gmail.com>
2025-11-15 13:40:34 +0530

    make other changes

---
+++ js/components/taskCard.js
@@ -0,0 +1,17 @@
+// taxkCard.innerHTML=`<h1 id="Titel">${}</h1>
+//                     <div>
+//                         <button class="button" id="Edit Task">${}</button>
+//                     </div>
+//                     <p id="description">${}.</p>
+//                     <div>
+//                         <time id="created_time" datetime="2025-11-13">${}</time>
+//                     </div>
+//                     <div>
+//                         <input type="checkbox" id="complit_box" value="complited">
+//                         <label for="apple"> complete </label>
+//                         <div>
+//                             <button class="button" id="dlt"> Delete </button>
+//                         </div>
+//                     </div>`
+
+
